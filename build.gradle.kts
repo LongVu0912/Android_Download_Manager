09c9a9baa4d5b77625854536c513bb7953a838bb
LongVu2190 <hoanglongvu912@gmail.com>
2024-04-12 12:34:36 +0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.1")
+        classpath("io.realm:realm-gradle-plugin:10.18.0")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.3.1" apply false
+    id("com.android.library") version "8.3.1" apply false
+    id ("org.jetbrains.kotlin.android") version "1.8.0" apply false
+}
+
+task("clean", Delete::class) {
+    delete = setOf(rootProject.buildDir)
+}
