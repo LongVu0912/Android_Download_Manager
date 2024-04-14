c577372872a00e6755bb903da958f26c3ccd428c
LongVu2190 <hoanglongvu912@gmail.com>
2024-04-14 14:52:55 +0700

    convert to sqlite and fix permission bug

---
+++ app/src/main/java/com/example/download_manager/DatabaseHelper.java
@@ -0,0 +1,91 @@
+package com.example.download_manager;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "downloadManager.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_DOWNLOAD_TABLE = "CREATE TABLE DownloadModel (downloadId INTEGER PRIMARY KEY, title TEXT, file_path TEXT, progress TEXT, status TEXT, file_size TEXT, is_paused INTEGER)";
+        db.execSQL(CREATE_DOWNLOAD_TABLE);
+
+//        String ADD_DOWNLOAD_TABLE = "INSERT INTO DownloadModel (title, file_path, progress, status, file_size, is_paused) VALUES ('Test', 'file_path', '50', 'FINISHED', '100', 0)";
+//        db.execSQL(ADD_DOWNLOAD_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS DownloadModel");
+        onCreate(db);
+    }
+    public Number getCurrentMaxId() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT MAX(downloadId) FROM DownloadModel", null);
+        cursor.moveToFirst();
+        Number maxId = cursor.getInt(0);
+        cursor.close();
+        db.close();
+        return maxId;
+    }
+    public void addDownload(DownloadModel downloadModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("title", downloadModel.getTitle());
+        values.put("file_path", downloadModel.getFile_path());
+        values.put("progress", downloadModel.getProgress());
+        values.put("status", downloadModel.getStatus());
+        values.put("file_size", downloadModel.getFile_size());
+        values.put("is_paused", downloadModel.getIs_paused());
+
+        db.insert("DownloadModel", null, values);
+        db.close();
+    }
+    @SuppressLint("Range")
+    public List<DownloadModel> getAllDownloads() {
+        List<DownloadModel> downloadList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM DownloadModel", null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                DownloadModel downloadModel = new DownloadModel();
+                downloadModel.setDownloadId(cursor.getLong(cursor.getColumnIndex("downloadId")));
+                downloadModel.setTitle(cursor.getString(cursor.getColumnIndex("title")));
+                downloadModel.setFile_path(cursor.getString(cursor.getColumnIndex("file_path")));
+                downloadModel.setProgress(cursor.getString(cursor.getColumnIndex("progress")));
+                downloadModel.setStatus(cursor.getString(cursor.getColumnIndex("status")));
+                downloadModel.setFile_size(cursor.getString(cursor.getColumnIndex("file_size")));
+                downloadModel.setIs_paused(cursor.getInt(cursor.getColumnIndex("is_paused")) != 0);
+
+                downloadList.add(downloadModel);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return downloadList;
+    }
+    public void deleteAllDownloads() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM DownloadModel");
+        db.close();
+    }
+}
---
+++ app/src/main/java/com/example/download_manager/DownloadModel.java
@@ -0,0 +1,77 @@
+package com.example.download_manager;
+
+public class DownloadModel {
+    private int id;
+    private String status;
+    private String title;
+    private String file_size;
+    private String progress;
+    private boolean is_paused;
+    private long downloadId;
+    private String file_path;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFile_size() {
+        return file_size;
+    }
+
+    public void setFile_size(String file_size) {
+        this.file_size = file_size;
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
+
+    public boolean getIs_paused() {
+        return is_paused;
+    }
+
+    public void setIs_paused(boolean is_paused) {
+        this.is_paused = is_paused;
+    }
+
+    public long getDownloadId() {
+        return downloadId;
+    }
+
+    public void setDownloadId(long downloadId) {
+        this.downloadId = downloadId;
+    }
+
+    public String getFile_path() {
+        return file_path;
+    }
+
+    public void setFile_path(String file_path) {
+        this.file_path = file_path;
+    }
+}
